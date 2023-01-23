88ed2c2a69b1cab4d3ce4f798414c08dcdff8520
João Mangueira <jamjs7@gmail.com>
2023-01-23 11:31:21 -0300

    tela quase pronta

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -1,5 +1,13 @@
+import { HabitDay } from './HabitDay';
+import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning';
+
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
 export function SummaryTable() {
+
+  const summaryDates = generateDatesFromYearBeginning();
+
+  const minimunSummaryDatesSize = 18 * 7;
+  const amountOfDaysToFill = minimunSummaryDatesSize - summaryDates.length;
 
   return (
     <div className="w-full flex">
@@ -14,8 +22,21 @@ export function SummaryTable() {
 
       </div>
 
-      <div className="grid grid-rows-7 grid-flow-row gap-3">
-        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+      <div className="grid grid-rows-7 grid-flow-col gap-3">
+        {summaryDates.map(date => {
+          return <HabitDay key={date.toString()}/>
+        })}
+
+        {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill })
+          .map((_, index) => {
+          return (
+            <div
+              key={index}
+              className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+            />
+          );
+        })}
+
       </div>
     </div>
 
